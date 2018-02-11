34bd441b44c50ffc6644255fbfb1bdf2b1c61e8d
Borys Bartsyts <bartzitz@gmail.com>
2018-02-11 23:05:25 +0200

    Skip docker completion download when the files already downloaded

--- docker/install.sh
+++ docker/install.sh
@@ -2,12 +2,12 @@
 
 mkdir -p "$HOME/.docker/completions"
 
-if which docker-compose >/dev/null 2>&1; then
+if [ ! -f "$HOME/.docker/completions/_docker-compose" ]; then
   curl -sL https://raw.githubusercontent.com/docker/compose/master/contrib/completion/zsh/_docker-compose \
     -o "$HOME/.docker/completions/_docker-compose"
 fi
 
-if which docker >/dev/null 2>&1; then
+if [ ! -f "$HOME/.docker/completions/_docker" ]; then
   curl -sL https://raw.githubusercontent.com/docker/cli/master/contrib/completion/zsh/_docker \
     -o "$HOME/.docker/completions/_docker"
 fi
