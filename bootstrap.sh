353926e5bd0801904831c619e5884fb568f86179
Borys Bartsyts <bartzitz@gmail.com>
2018-06-27 19:30:08 +0300

    Simplified bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,45 +1,12 @@
 #!/bin/sh
-#
-# This script will symlink .dotfiles/*/*.symlink files to home dir
-
-cd "$(dirname "$0")/.."
-DOTFILES_ROOT=$(pwd -P)
 
 set -e
 
-install_deps() {
-  echo "=== installing deps..."
-  find -H "$DOTFILES_ROOT" -maxdepth 3 -name 'install.sh' -not -path '*.git*' |
-    while read -r installer; do
-      echo "running ${installer}..."
-      sh -c "$installer"
-    done
-}
+DOTFILES_ROOT=$(pwd -P)
 
-link_file() {
-  if [ -e "$2" ]; then
-    if [ "$(readlink "$2")" = "$1" ]; then
-      echo "skipped $1"
-      return 0
-    else
-      mv "$2" "$2.backup"
-      echo "moved $2 to $2.backup"
-    fi
-  fi
-  ln -sf "$1" "$2"
-  echo "linked $1 to $2"
-}
+./docker/install.sh
+./zgen/install.sh
 
-link_dotfiles() {
-  echo "=== symlinking dotfiles..."
-  find -H "$DOTFILES_ROOT" -maxdepth 3 -name '*.symlink' -not -path '*.git*' |
-    while read -r src; do
-      dst="$HOME/.$(basename "${src%.*}")"
-      link_file "$src" "$dst"
-    done
-}
-
-install_deps
-link_dotfiles
+ln -sf "$DOTFILES_ROOT/zsh/.zshrc" "$HOME"
 
 echo 'all done!'
