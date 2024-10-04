d923213a74cf56ae4e70c5fb31a142aa57c63954
Borys Bartsyts <bbartsyts@n-ix.com>
2024-10-04 17:04:50 +0300

    Switch to catppuccin-frappe theme

---
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -0,0 +1,14 @@
+return {
+  "catppuccin/nvim",
+  name = "catppuccin",
+  priority = 1000,
+  config = function()
+    require("catppuccin").setup {
+      dim_inactive = {
+        enabled = true, -- dims the background color of inactive window
+        shade = "dark",
+        percentage = 0.15, -- percentage of the shade to apply to the inactive window
+      },
+    }
+  end,
+}
