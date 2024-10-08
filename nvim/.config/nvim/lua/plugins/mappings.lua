e0b23cc0cf58deb790f34d22e92560f470bd46d1
Borys Bartsyts <bbartsyts@n-ix.com>
2024-10-08 17:46:02 +0300

    Nvim - Swap <Leader>t (ToggleTerm) with <Leader>T (vim-test)

---
+++ nvim/.config/nvim/lua/plugins/mappings.lua
@@ -0,0 +1,35 @@
+---@type LazySpec
+return {
+  "AstroNvim/astrocore",
+  ---@type AstroCoreOpts
+  opts = function(_, opts)
+    local maps = opts.mappings
+
+    -- Remap vim-test maps from T to t (ToggleTerm)
+    local t = maps.n["<Leader>T"]
+    local tf = maps.n["<Leader>Tf"]
+    local tn = maps.n["<Leader>Tn"]
+    local tv = maps.n["<Leader>Tv"]
+
+    maps.n["<Leader>T"] = maps.n["<Leader>t"]
+    maps.n["<Leader>Tf"] = maps.n["<Leader>tf"]
+    maps.n["<Leader>Th"] = maps.n["<Leader>th"]
+    maps.n["<Leader>Tn"] = maps.n["<Leader>tn"]
+    maps.n["<Leader>Tp"] = maps.n["<Leader>tp"]
+    maps.n["<Leader>Tv"] = maps.n["<Leader>tv"]
+
+    maps.n["<Leader>t"] = t
+    maps.n["<Leader>tf"] = tf
+    maps.n["<Leader>tn"] = tn
+    maps.n["<Leader>tv"] = tv
+    maps.n["<Leader>th"] = nil
+    maps.n["<Leader>tp"] = nil
+    maps.n["<Leader>tc"] = maps.n["<Leader>Tc"]
+    maps.n["<Leader>tl"] = maps.n["<Leader>Tl"]
+    maps.n["<Leader>ts"] = maps.n["<Leader>Ts"]
+
+    maps.n["<Leader>Tc"] = nil
+    maps.n["<Leader>Tl"] = nil
+    maps.n["<Leader>Ts"] = nil
+  end,
+}
